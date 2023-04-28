083ebbfe48197807b31ae2932b0418ec15620bab
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-04-27 22:03:23 -0400

    form html addes usign tailwind,daisy and modal

---
+++ src/routes/forms/+scheduleFormClient-Medic.svelte
@@ -0,0 +1,23 @@
+<label for="my-modal-3" class="btn">open modal</label>
+<input type="checkbox" id="my-modal-3" class="modal-toggle" />
+<div class="modal">
+    <div class="modal-box relative">
+        <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+        <form>
+            <h1 class="title">Book Medical Appointment</h1>
+            <div class="form-control">
+                <label class="label">Fecha:</label>
+                <input class="input" type="date" name="fecha" required>
+            </div>
+            <div class="form-control">
+                <label class="label">Hora:</label>
+                <input class="input" type="time" name="hora" required>
+            </div>
+            <div class="form-control">
+                <label class="label">Descripción:</label>
+                <textarea class="textarea" name="descripcion" rows="4"></textarea>
+            </div>
+            <button class="btn2">Schedule</button>
+        </form>
+    </div>
+</div>
