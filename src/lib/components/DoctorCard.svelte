94f690aca340729f747b0a0757eb981438818d76
Santihs <75571308+Santihs@users.noreply.github.com>
2023-04-29 11:49:46 -0400

    Doctor card and categories created

---
+++ src/lib/components/DoctorCard.svelte
@@ -0,0 +1,20 @@
+<div class="card lg:card-side bg-base-100 shadow-xl mb-4">
+	<figure>
+		<div class="avatar">
+			<div class="w-24 rounded">
+				<img src="./src/lib/images/default.png" alt="default" />
+			</div>
+		</div>
+	</figure>
+	<div class="card-body">
+		<h2 class="card-title">Fady Khoury-Collado, MDS</h2>
+		<p>
+			<b>Specialities:</b> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, reiciendis!
+		</p>
+		<div>
+			<a class="link link-hover link-info">More specialities</a>
+			<br />
+			<a class="link link-hover link-info"><i class="bi bi-telephone-fill" /> 212-395-3210</a>
+		</div>
+	</div>
+</div>
