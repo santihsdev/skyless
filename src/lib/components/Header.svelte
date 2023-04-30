9e34e0e90a7ed89711295b3cba40440a909ccf06
gabrielagonzalesvillarroel <Gabriela.Gonzales.60113@usalesiana.edu.bo>
2023-04-30 00:04:26 -0400

    Improve search buttons in header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -20,33 +20,43 @@
 				tabindex="0"
 				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
 			>
-			<li tabindex="0">
-				<a class="justify-between">
-					Find a doctor
-					<svg
-						class="fill-current"
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg
-					>
-				</a>
-				<ul class="p-2 bg-accent-focus">
-					<h4>Search for a Doctor</h4>
-					<div class="flex w-full">
-						<div class="grid h-20 flex-grow card rounded-box place-items-center">
-							<div class="form-control">
-								<input type="text" placeholder="Search" class="input input-bordered" />
+				<li tabindex="0">
+					<a class="justify-between">
+						Find a doctor
+						<svg
+							class="fill-current"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg
+						>
+					</a>
+					<ul class="p-2 bg-accent-focus">
+						<h4>Search for a Doctor</h4>
+						<div class="form-control">
+							<div class="input-group">
+								<input type="text" placeholder="Search…" class="input input-bordered" />
+								<button class="btn btn-square">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="h-6 w-6"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+										><path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="2"
+											d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+										/></svg
+									>
+								</button>
 							</div>
 						</div>
-						<div class="grid h-20 flex-grow card rounded-box place-items-center">
-							<button class="btn btn-outline text-white">Search</button>
-						</div>
-					</div>
-					<button class="btn btn-link text-white">View all doctors</button>
-				</ul>
-			</li>
+						<button class="btn btn-link text-white">View all doctors</button>
+					</ul>
+				</li>
 				<li tabindex="0">
 					<a class="justify-between">
 						About us
@@ -84,14 +94,24 @@
 				</a>
 				<ul class="p-2 bg-accent-focus">
 					<h4>Search for a Doctor</h4>
-					<div class="flex w-full">
-						<div class="grid h-20 flex-grow card rounded-box place-items-center">
-							<div class="form-control">
-								<input type="text" placeholder="Search" class="input input-bordered" />
-							</div>
-						</div>
-						<div class="grid h-20 flex-grow card rounded-box place-items-center">
-							<button class="btn btn-outline text-white">Search</button>
+					<div class="form-control">
+						<div class="input-group">
+							<input type="text" placeholder="Search…" class="input input-bordered" />
+							<button class="btn btn-square">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="h-6 w-6"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									><path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+									/></svg
+								>
+							</button>
 						</div>
 					</div>
 					<button class="btn btn-link text-white">View all doctors</button>
@@ -118,10 +138,20 @@
 	</div>
 	<div class="navbar-end">
 		<a class="btn btn-ghost">
-			<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25q" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-				<path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="25"
+				height="25q"
+				fill="currentColor"
+				class="bi bi-person"
+				viewBox="0 0 16 16"
+			>
+				<path
+					d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"
+				/>
 			</svg>
-			
-			&nbsp; Login</a>
+
+			&nbsp; Login</a
+		>
 	</div>
 </div>
