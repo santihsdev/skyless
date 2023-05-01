2eb1e936b3d1e8741a1fa8bc29b336cb8d976c26
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-05-01 19:55:29 -0400

    server create api file added

---
+++ src/routes/api/appoinments/create/+server.js
@@ -0,0 +1,15 @@
+import {client} from "$lib/database/connector.js";
+import { json } from '@sveltejs/kit';
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST(event) {
+    const body = await event.request.formData();
+
+    const result = await client
+        .query(
+            "INSERT INTO appointment_form (date, hour, description, id_doctor, id_user) VALUES ($1, $2, $3, $4, $5)",
+            [body.get("date"), body.get("hour"), body.get("description"), body.get("id_doctor"), body.get("id_user")]
+        );
+
+    return json(result.rows[0]);
+}
