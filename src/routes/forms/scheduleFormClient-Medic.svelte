f1e5235c8c6acba00e0debe81f915369abbaac3e
agonzalesb107 <andres.gonzales.60093@usalesiana.edu.bo>
2023-05-01 03:03:59 -0400

    added functions to write on data base

--- src/routes/forms/scheduleFormClient-Medic.svelte
+++ src/routes/forms/scheduleFormClient-Medic.svelte
@@ -1,131 +1,151 @@
 <script>
-    let fecha;
-    let hora;
-    let descripcion;
+	import { Client } from 'pg';
+    
+	console.log('asd');
+	let dateValue;
+	let hourValue;
+	let descriptionValue;
+	let id_doctor;
+	let id_user;
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        fecha = formData.get('fecha');
-        hora = formData.get('hora');
-        descripcion = formData.get('descripcion');
+	const pool = new Client({
+		user: 'keycloak',
+		host: '172.18.0.2',
+		database: 'postgres',
+		password: 'password',
+		port: 5432
+	});
 
-        if (fecha && hora && descripcion) {
+	pool.connect();
 
-            console.log('Fecha:', fecha);
-            console.log('Hora:', hora);
-            console.log('Descripción:', descripcion);
-            closeModal();
-        }
-    }
-    function closeModal() {
-        const modalCheckbox = document.getElementById('my-modal-3');
-        modalCheckbox.checked = false;
-    }
+	pool.query('SELECT * FROM mi_tabla', (err, res) => {
+		console.log(err, res);
+		pool.end();
+	});
+
+	async function crearCita(date, hour, description, id_doctor, id_user) {
+		const query =
+			'INSERT INTO appointment_form (date, hour, description, id_doctor, id_user) VALUES ($1, $2, $3, $4, $5)';
+		const values = [date, hour, description, id_doctor, id_user];
+
+		try {
+			const res = await pool.query(query, values);
+			console.log('Se ha creado una nueva cita en la base de datos');
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		dateValue = formData.get('fecha');
+		hourValue = formData.get('hora');
+		descriptionValue = formData.get('descripcion');
+		id_doctor = 1;
+		id_user = 2;
+
+		crearCita(dateValue, hourValue, descriptionValue, id_doctor, id_user);
+	}
 </script>
-
 
 <label for="my-modal-3" class="btn">schedule now</label>
 <input type="checkbox" id="my-modal-3" class="modal-toggle" />
 
 <div class="modal">
-    <div class="modal-box relative">
-        <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-        <form on:submit={handleSubmit}>
-            <h1 class="title">Book Medical Appointment</h1>
-            <div class="form-control">
-                <label class="label">Fecha:</label>
-                <input class="input" type="date" name="fecha" required>
-            </div>
-            <div class="form-control">
-                <label class="label">Hora:</label>
-                <input class="input" type="time" name="hora" required>
-            </div>
-            <div class="form-control">
-                <label class="label">Descripción:</label>
-                <textarea class="textarea" name="descripcion" rows="4"></textarea>
-            </div>
+	<div class="modal-box relative">
+		<label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+		<form on:submit={handleSubmit}>
+			<h1 class="title">Book Medical Appointment</h1>
+			<div class="form-control">
+				<label class="label">Fecha:</label>
+				<input class="input" type="date" name="fecha" required />
+			</div>
+			<div class="form-control">
+				<label class="label">Hora:</label>
+				<input class="input" type="time" name="hora" required />
+			</div>
+			<div class="form-control">
+				<label class="label">Descripción:</label>
+				<textarea class="textarea" name="descripcion" rows="4" />
+			</div>
 
-            <button type="submit" class="btn2">Schedule</button>
-        </form>
-    </div>
+			<button type="submit" class="btn2">Schedule</button>
+		</form>
+	</div>
 </div>
 
-
 <style>
-    form {
-        max-width: 400px;
-        margin: 0 auto;
-        padding: 2rem;
-        background-color: #ffffff;
-        color: black;
-        font-family: Arial,serif;
-    }
-    .form-control {
-        margin-bottom: 1rem;
-    }
-    .label {
-        display: block;
-        font-weight: bold;
-        margin-bottom: 0.5rem;
-    }
-    .title{
-        display: block;
-        font-weight: bold;
-        font-size: 1.6rem;
-        margin: auto;
-        text-align: center;
-    }
-    input[type=date],
-    input[type=time] {
-        width: 100%;
-        padding: 0.5rem;
-        border: 1px solid #ccc;
-        border-radius: 0.25rem;
-        font-size: 1rem;
-        line-height: 1.5;
-        background-color: #fff;
-        color: #000;
-    }
-    input[type=date]::-webkit-calendar-picker-indicator,
-    input[type=time]::-webkit-calendar-picker-indicator {
-        background-color: #79ccd1;
-        color: #fff;
-        border: none;
-        border-radius: 0.25rem;
-        padding: 0.25rem;
-    }
+	form {
+		max-width: 400px;
+		margin: 0 auto;
+		padding: 2rem;
+		background-color: #ffffff;
+		color: black;
+		font-family: Arial, serif;
+	}
+	.form-control {
+		margin-bottom: 1rem;
+	}
+	.label {
+		display: block;
+		font-weight: bold;
+		margin-bottom: 0.5rem;
+	}
+	.title {
+		display: block;
+		font-weight: bold;
+		font-size: 1.6rem;
+		margin: auto;
+		text-align: center;
+	}
+	input[type='date'],
+	input[type='time'] {
+		width: 100%;
+		padding: 0.5rem;
+		border: 1px solid #ccc;
+		border-radius: 0.25rem;
+		font-size: 1rem;
+		line-height: 1.5;
+		background-color: #fff;
+		color: #000;
+	}
+	input[type='date']::-webkit-calendar-picker-indicator,
+	input[type='time']::-webkit-calendar-picker-indicator {
+		background-color: #79ccd1;
+		color: #fff;
+		border: none;
+		border-radius: 0.25rem;
+		padding: 0.25rem;
+	}
 
-    .textarea {
-        width: 100%;
-        padding: 0.5rem;
-        border: 1px solid #cccccc;
-        border-radius: 0.25rem;
-        font-size: 1rem;
-        line-height: 1.5;
-        background-color: rgba(231, 231, 231, 0.54);
-
-    }
-    .textarea {
-        resize: vertical;
-        background-color: white;
-    }
-    .btn {
-        background-color: #79ccd1;
-        color: #fff;
-    }
-    .btn2 {
-        display: block;
-        margin-top: 1.5rem;
-        background-color: #79ccd1;
-        color: #fff;
-        border: none;
-        border-radius: 0.25rem;
-        padding: 0.5rem 1rem;
-        font-size: 1rem;
-        cursor: pointer;
-        margin-left: 13.5rem;
-    }
+	.textarea {
+		width: 100%;
+		padding: 0.5rem;
+		border: 1px solid #cccccc;
+		border-radius: 0.25rem;
+		font-size: 1rem;
+		line-height: 1.5;
+		background-color: rgba(231, 231, 231, 0.54);
+	}
+	.textarea {
+		resize: vertical;
+		background-color: white;
+	}
+	.btn {
+		background-color: #79ccd1;
+		color: #fff;
+	}
+	.btn2 {
+		display: block;
+		margin-top: 1.5rem;
+		background-color: #79ccd1;
+		color: #fff;
+		border: none;
+		border-radius: 0.25rem;
+		padding: 0.5rem 1rem;
+		font-size: 1rem;
+		cursor: pointer;
+		margin-left: 13.5rem;
+	}
 </style>
-
-
