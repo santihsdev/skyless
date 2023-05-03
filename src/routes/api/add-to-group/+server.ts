1155403514ca650ca09797f5465d49a7a641f609
Santi Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-05-03 19:32:31 -0400

    Add update, and create doctor in db

--- src/routes/api/add-to-group/+server.ts
+++ src/routes/api/add-to-group/+server.ts
@@ -17,7 +17,6 @@ const getGroupId = async (group: string, token: string): Promise<number> => {
 /** @type {import('./$types').RequestHandler} */
 export const POST = async ({ request }) => {
 	const { token, idUser, groupName } = await request.json();
-	console.log(token);
 
 	const groupId = await getGroupId(groupName, token);
 
@@ -31,7 +30,6 @@ export const POST = async ({ request }) => {
 			}
 		}
 	);
-	// const result = await resp.json();
 	
 	if (resp.status == 204){
 		return json({ hello: 'world' });
---
+++ src/routes/api/doctors/create/+server.ts
@@ -0,0 +1,16 @@
+import { client } from '$lib/database/connector';
+import { json } from '@sveltejs/kit';
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+	const { id, name, lastName, cellphone, speciality, email } = await request.json();
+
+	const result = await client.query(
+		'INSERT INTO public.doctor(id, name, first_name, last_name, cellphone, speciality, email) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+		[id, `${name} ${lastName}`, name, lastName, cellphone, speciality, email]
+	);
+
+	console.log(result);
+
+	return json(result.rowCount);
+}
