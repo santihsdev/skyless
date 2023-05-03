7cdece3c23376b9605a81236ef476b4e8bbc266a
Santi Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-05-03 19:32:31 -0400

    Add update, and create doctor in db

---
+++ src/routes/api/doctors/update/+server.ts
@@ -0,0 +1,16 @@
+import { client } from '$lib/database/connector';
+import { json } from '@sveltejs/kit';
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+	const { id, name, lastName, cellphone, speciality, email } = await request.json();
+
+	const result = await client.query(
+		'UPDATE public.doctor SET id=$1, name=$2, first_name=$3, last_name=$4, cellphone=$5, speciality=$6, email=$7 WHERE id=$1;',
+		[id, `${name} ${lastName}`, name, lastName, cellphone, speciality, email]
+	);
+
+	console.log(result);
+
+	return json(result.rowCount);
+}
