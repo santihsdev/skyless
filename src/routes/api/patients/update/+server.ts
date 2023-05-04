086a682caeb6dbbd3800e68371be7fb516ee4321
Santi Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-05-03 20:00:28 -0400

    Endpoint to update user in keycloak created

---
+++ src/routes/api/patients/update/+server.ts
@@ -0,0 +1,31 @@
+import { client } from '$lib/database/connector';
+import { json } from '@sveltejs/kit';
+
+export type Client = {
+	email: string;
+	firstName: string;
+	lastName: string;
+	username?: string;
+};
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+	const { idUser, token, client } = await request.json();
+
+	const userValues: Client = client;
+
+	const resp = await fetch(`http://0.0.0.0:8080/admin/realms/test/users/${idUser}`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`
+		},
+		body: JSON.stringify(userValues)
+	});
+
+	console.log(resp.status);
+
+	if (resp.status == 204) {
+		return json({ ok: resp.status });
+	}
+}
