262f25761f9c90531c216f7a2a69a75398081b0a
Maria Rene Huanca Aliaga <maria.huanca.60095@usalesiana.edu.bo>
2023-05-04 19:28:16 -0400

    added icons in .svelte files and clean code icons in components files

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,20 +1,17 @@
+<script>
+	import ArrowDownIcon from "../../icons/ArrowDownIcon.svelte";
+	import ArrowRightIcon from "../../icons/ArrowRightIcon.svelte";
+	import LoginIcon from "../../icons/LoginIcon.svelte";
+	import SearchIcon from "../../icons/SearchIcon.svelte";
+	import ListIcon from "../../icons/ListIcon.svelte";
+
+</script>
+
 <div class="navbar bg-base-100" style="position: relative; z-index: 10;">
 	<div class="navbar-start">
 		<div class="dropdown">
 			<label tabindex="0" class="btn btn-ghost lg:hidden">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/></svg
-				>
+				<ListIcon/>
 			</label>
 			<ul
 				tabindex="0"
@@ -23,14 +20,7 @@
 				<li tabindex="0">
 					<a class="justify-between">
 						Find a doctor
-						<svg
-							class="fill-current"
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg
-						>
+						<ArrowRightIcon/>
 					</a>
 					<ul class="p-2 bg-accent-focus">
 						<h4>Search for a Doctor</h4>
@@ -38,19 +28,7 @@
 							<div class="input-group">
 								<input type="text" placeholder="Search…" class="input input-bordered" />
 								<button class="btn btn-square">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										class="h-6 w-6"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										><path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
-											d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-										/></svg
-									>
+									<SearchIcon/>
 								</button>
 							</div>
 						</div>
@@ -60,14 +38,7 @@
 				<li tabindex="0">
 					<a class="justify-between">
 						About us
-						<svg
-							class="fill-current"
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg
-						>
+						<ArrowRightIcon/>
 					</a>
 					<ul class="p-2 bg-accent-focus">
 						<li><button class="btn btn-outline btn-wide text-white">Our mission</button></li>
@@ -83,14 +54,7 @@
 			<li tabindex="0">
 				<a>
 					Find a doctor
-					<svg
-						class="fill-current"
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						viewBox="0 0 24 24"
-						><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg
-					>
+					<ArrowDownIcon/>
 				</a>
 				<ul class="p-2 bg-accent-focus">
 					<h4>Search for a Doctor</h4>
@@ -98,19 +62,7 @@
 						<div class="input-group">
 							<input type="text" placeholder="Search…" class="input input-bordered" />
 							<button class="btn btn-square">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									class="h-6 w-6"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									><path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-									/></svg
-								>
+								<SearchIcon/>
 							</button>
 						</div>
 					</div>
@@ -120,14 +72,7 @@
 			<li tabindex="0">
 				<a>
 					About us
-					<svg
-						class="fill-current"
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						viewBox="0 0 24 24"
-						><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg
-					>
+					<ArrowDownIcon/>
 				</a>
 				<ul class="p-2 bg-accent-focus flex-auto w-100">
 					<button class="btn btn-outline btn-wide text-white">Our mission</button>
@@ -138,20 +83,8 @@
 	</div>
 	<div class="navbar-end">
 		<a class="btn btn-ghost" href="/login">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="25"
-				height="25q"
-				fill="currentColor"
-				class="bi bi-person"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"
-				/>
-			</svg>
-
-			&nbsp; Login</a
-		>
+			<LoginIcon/>
+			&nbsp; Login
+		</a>
 	</div>
 </div>
