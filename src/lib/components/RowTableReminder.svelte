8f82bbe22299ffc146034bc02ba21a620d231032
agonzalesb107 <andres.gonzales.60093@usalesiana.edu.bo>
2023-05-05 08:51:23 -0400

    Added buttons html to RowTableRemainder

--- src/lib/components/RowTableReminder.svelte
+++ src/lib/components/RowTableReminder.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	let isDone: boolean = false;
 	export let idDoctor: number = 0;
 	export let idUser: number = 0;
@@ -21,8 +22,8 @@
 				</div>
 			</div>
 			<div>
-				<div class="font-bold">{idDoctor} Dr. Marcus Ryan</div>
-				<div class="text-sm opacity-50">Oncologist</div>
+				<div class="font-bold">{idDoctor} {nameDoctor}</div>
+				<div class="text-sm opacity-50">{speciality}</div>
 			</div>
 		</div>
 	</td>
@@ -33,6 +34,29 @@
 	</td>
 	<td>{description}</td>
 	<th>
-		<button class="btn btn-ghost btn-xs" disabled={isDone} >details</button>
+		<button class="my-button">Edit</button>
+		<button class="delete-btn" on:click={() => deleteAppointment(idDoctor)}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="icon icon-tabler icon-tabler-trash"
+				width="19"
+				height="19"
+				viewBox="0 0 24 24"
+				stroke-width="5.5"
+				stroke="#2c3e50"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" />
+				<line x1="4" y1="7" x2="20" y2="7" />
+				<line x1="10" y1="11" x2="10" y2="17" />
+				<line x1="14" y1="11" x2="14" y2="17" />
+				<path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+				<path d="M9 4h6" />
+			</svg>
+		</button>
 	</th>
 </tr>
+
+
