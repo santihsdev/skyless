0234e84a70ccd1efccf3a8fc5f7c58a335b2f18f
gabrielagonzalesvillarroel <Gabriela.Gonzales.60113@usalesiana.edu.bo>
2023-05-05 08:53:57 -0400

    Refactor code in HomePatient

--- src/routes/(home)/patient/[id]/+page.svelte
+++ src/routes/(home)/patient/[id]/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-	import Homepatient from "$lib/components/Homepatient.svelte";
+	import HomePatient from "$lib/components/HomePatient.svelte";
 	let token ="patient"
 </script>
 
 {#if token.length != 0}
-<Homepatient/>
+<HomePatient/>
 {/if}
 
