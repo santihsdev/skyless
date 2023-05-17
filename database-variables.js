60be8bf2e0768c81cce66da437200734f2b8633e
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-05-17 17:58:51 -0400

    file added with the bd info in variables

---
+++ database-variables.js
@@ -0,0 +1,12 @@
+const database = {
+    table_doctor :"doctor",
+    doctor_table: "bd_schema.doctor",
+    appointment_table: "bd_schema.appointment_form",
+    database_schema_name: "bd_schema",
+    database_port: "8000",
+    data_base_name: 'postgres',
+    data_base_user: 'keycloak',
+    data_base_password: 'password'
+};
+
+module.exports = database;
