1da4b6cefe0e654652276e99d05d79ce535fae34
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-05-23 10:35:00 -0400

    doctor table information updated with the new columuns info

---
+++ data-base/postgres/sql/V1_08__add_speciality_values_doctor_table.sql
@@ -0,0 +1,35 @@
+UPDATE bd_schema.doctor
+SET speciality_id = 'ESP001'
+WHERE speciality = 'plastic surgery';
+
+UPDATE bd_schema.doctor
+SET speciality_id = 'ESP002'
+WHERE speciality = 'general medicine';
+
+UPDATE bd_schema.doctor
+SET speciality_id = 'ESP003'
+WHERE speciality = 'obstetrics and gynecology';
+
+UPDATE bd_schema.doctor
+SET speciality_id = 'ESP004'
+WHERE speciality = 'odontology';
+
+UPDATE bd_schema.doctor
+SET speciality_id = 'ESP005'
+WHERE speciality = 'ophthalmology';
+
+UPDATE bd_schema.doctor
+SET speciality_id = 'ESP006'
+WHERE speciality = 'orthorhinolaryngology';
+
+UPDATE bd_schema.doctor
+SET speciality_id = 'ESP007'
+WHERE speciality = 'pediatrician';
+
+UPDATE bd_schema.doctor
+SET speciality_id = 'ESP008'
+WHERE speciality = 'traumatology';
+
+UPDATE bd_schema.doctor
+SET speciality_id = 'ESP009'
+WHERE speciality = 'urology';
