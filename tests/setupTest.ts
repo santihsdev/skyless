699be2b61ea9785e662e44c47b464ae11a6fb7f8
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-05-23 16:56:29 -0400

    Test example by SideMenu created

---
+++ tests/setupTest.ts
@@ -0,0 +1,75 @@
+import '@testing-library/jest-dom';
+import matchers from '@testing-library/jest-dom/matchers';
+import { expect, vi } from 'vitest';
+import type { Navigation, Page } from '@sveltejs/kit';
+import { readable } from 'svelte/store';
+import type * as environment from '$app/environment';
+import type * as navigation from '$app/navigation';
+import type * as stores from '$app/stores';
+
+// Add custom jest matchers
+expect.extend(matchers);
+
+// Mock SvelteKit runtime module $app/environment
+vi.mock('$app/environment', (): typeof environment => ({
+	browser: false,
+	dev: true,
+	prerendering: false
+}));
+
+// Mock SvelteKit runtime module $app/navigation
+vi.mock('$app/navigation', (): typeof navigation => ({
+	afterNavigate: () => {},
+	beforeNavigate: () => {},
+	disableScrollHandling: () => {},
+	goto: () => Promise.resolve(),
+	invalidate: () => Promise.resolve(),
+	invalidateAll: () => Promise.resolve(),
+	prefetch: () => Promise.resolve(),
+	prefetchRoutes: () => Promise.resolve()
+}));
+
+// Mock SvelteKit runtime module $app/stores
+vi.mock('$app/stores', (): typeof stores => {
+	const getStores = () => {
+		const navigating = readable<Navigation | null>(null);
+		const page = readable<Page>({
+			url: new URL('http://localhost'),
+			params: {},
+			route: {
+				id: null
+			},
+			status: 200,
+			error: null,
+			data: {},
+			form: undefined
+		});
+		const updated = { subscribe: readable(false).subscribe, check: () => false };
+
+		return { navigating, page, updated };
+	};
+
+	const page: typeof stores.page = {
+		subscribe(fn) {
+			return getStores().page.subscribe(fn);
+		}
+	};
+	const navigating: typeof stores.navigating = {
+		subscribe(fn) {
+			return getStores().navigating.subscribe(fn);
+		}
+	};
+	const updated: typeof stores.updated = {
+		subscribe(fn) {
+			return getStores().updated.subscribe(fn);
+		},
+		check: () => false
+	};
+
+	return {
+		getStores,
+		navigating,
+		page,
+		updated
+	};
+});
