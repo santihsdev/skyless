0e6259dae13fb63546642ed086c9fec380b36dfb
Maria Rene Huanca Aliaga <maria.huanca.60095@usalesiana.edu.bo>
2023-05-25 12:34:24 -0400

    test DoctorCard component

---
+++ tests/lib/components/DoctorCard.test.ts
@@ -0,0 +1,33 @@
+import { cleanup, render, screen } from '@testing-library/svelte';
+import DoctorCard from '../../../src/lib/components/DoctorCard.svelte';
+
+
+describe('DoctorCard component', () => {
+	it('should render the correct title', () => {
+		const { getByText } = render(DoctorCard, {
+			props: {
+				name: 'John',
+				lastName: 'Doe'
+			}
+		});
+
+		expect(getByText('Dr. John Doe')).toBeTruthy();
+	});
+
+    it('should render the correct link', () => {
+        const { getByText, getByTestId } = render(DoctorCard, {
+            props: {
+                cellphone: '123-456-7890'
+            }
+        });
+        expect(getByText('123-456-7890')).toBeTruthy();
+    });
+    it('should render the correct link', () => {
+        const { getByText} = render(DoctorCard);
+    
+        expect(getByText('More information')).toBeTruthy();
+    });
+});
+
+
+
