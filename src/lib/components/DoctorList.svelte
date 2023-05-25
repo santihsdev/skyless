02da99e3fe4cd1d9cd5b2c32966e125c62615a04
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-05-24 20:15:12 -0400

    Doctors listed by speciality

--- src/lib/components/DoctorList.svelte
+++ src/lib/components/DoctorList.svelte
@@ -3,21 +3,28 @@
 	import DoctorCard from './DoctorCard.svelte';
 	import type { Doctor } from '$lib/types/doctor';
 
-	export let speciality: string = 'Default';
+	export let specialityName: string = 'Default';
+	export let speciliatyId: string = 'id';
 	export let isOpen: boolean = false;
 	let doctors: Doctor[] = [];
 	onMount(async () => {
-		let resp = await fetch('/api/doctors');
+		let resp = await fetch(`/api/doctors/speciality?id=${speciliatyId}`);
 		doctors = await resp.json();
+		console.log(doctors);
 	});
+
+	const changeState = () => {
+		isOpen = !isOpen;
+	};
 </script>
 
-<div class="container mx-auto py-20 pl-12 pr-20 sm:ml-64">
+<div class="container mx-auto pt-16 pl-12 pr-20 sm:ml-64">
 	<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 	<div class="collapse border border-base-300 bg-base-100 rounded-box collapse-arrow">
 		<input type="checkbox" bind:checked={isOpen} />
-		<div class="collapse-title text-xl font-medium">
-			<h1 class="text-4xl font-bold">{speciality}</h1>
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div class="collapse-title text-xl font-medium" on:click={changeState}>
+			<h1 class="text-4xl font-bold">{specialityName}</h1>
 		</div>
 		<div class="collapse-content">
 			<!-- <DoctorCard /> -->
