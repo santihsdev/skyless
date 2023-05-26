9ffb419d606dd5416e30bdb1c2ba9acd1c2fbb1c
agonzalesb107 <andres.gonzales.60093@usalesiana.edu.bo>
2023-05-26 11:38:54 -0400

    added route to Appointments button

--- src/lib/components/SideMenu.svelte
+++ src/lib/components/SideMenu.svelte
@@ -26,11 +26,11 @@
 			</li>
 			<li>
 				<a
-					href="#"
+					href={`/patient/${$masterKey}/reminder`}
 					class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
 				>
 					<CalendarIcon />
-					<span class="flex-1 ml-3 whitespace-nowrap">Calendar</span>
+					<span class="flex-1 ml-3 whitespace-nowrap">Appointments</span>
 				</a>
 			</li>
 			<li>
