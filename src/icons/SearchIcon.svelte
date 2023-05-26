233d222795f942202e5224a86951984fdab7fe94
Maria Rene Huanca Aliaga <maria.huanca.60095@usalesiana.edu.bo>
2023-05-26 11:27:23 -0400

    modified search icon

--- src/icons/SearchIcon.svelte
+++ src/icons/SearchIcon.svelte
@@ -4,15 +4,15 @@
 </script>
 
 <svg
-	xmlns="http://www.w3.org/2000/svg"
-	class="h-6 w-6"
+	aria-hidden="true"
+	class="w-5 h-5 text-gray-500 dark:text-gray-400"
 	fill="none"
-	viewBox="0 0 24 24"
 	stroke="currentColor"
-	><path
+	viewBox="0 0 24 24"
+	xmlns="http://www.w3.org/2000/svg">
+    <path
 		stroke-linecap="round"
 		stroke-linejoin="round"
-		stroke-width="2"
-		d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-	/>
+		stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
+    </path>
 </svg>
