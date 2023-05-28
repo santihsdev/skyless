2cd8f2fd9a87f21d263f8572c7dd487d7fb273a5
Maria Rene Huanca Aliaga <maria.huanca.60095@usalesiana.edu.bo>
2023-05-27 23:16:16 -0400

    advance search

--- src/routes/api/doctors/search/+server.ts
+++ src/routes/api/doctors/search/+server.ts
@@ -3,15 +3,21 @@ import { doctor_table } from '$lib/database/database-variables';
 import { error, json } from '@sveltejs/kit';
 import type { RequestEvent, RequestHandler } from '../$types';
 
+
 export const GET: RequestHandler = async ({ url }: RequestEvent) => {
 	const name = url.searchParams.get('name') ?? '0';
 
-  console.log(name);
+  console.log("servers: ",name);
   
-	const result = await client.query(
-    `SELECT name FROM ${doctor_table} WHERE name LIKE $1;`, [`'%${name}%'`]
-	);
-  console.log(result);
+	console.log("query: ", `SELECT name FROM ${doctor_table} WHERE name LIKE '%${name}%';`);
 
-	return json(['hello']);
+	//let doctors: Doctor[] = result.rows.map(
+	const result = await client.query(
+    `SELECT * FROM ${doctor_table} WHERE name LIKE '%${name}%';`
+		//`SELECT name FROM ${doctor_table} WHERE name LIKE '$1';`, [`'%${name}%'`]
+		//`SELECT * FROM ${doctor_table} where id='DOC001';`
+	);
+  console.log("Resulttttt",result);
+		
+	return json(result.rows);
 };
