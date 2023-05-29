2689ac7e864461bfe40aecf355fd5e88dbe3b5da
Santi Huancu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2023-05-29 10:30:04 -0400

    Problems related to test and svelte fixed

--- tests/lib/components/DoctorCard.test.ts
+++ tests/lib/components/DoctorCard.test.ts
@@ -11,7 +11,7 @@ describe('DoctorCard component', () => {
 			}
 		});
 
-		expect(getByText('Dr. John Doe')).toBeTruthy();
+		expect(getByText('John Doe')).toBeTruthy();
 	});
 
     it('should render the correct link', () => {
