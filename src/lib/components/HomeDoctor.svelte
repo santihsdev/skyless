4f80ff448917d80c471a428f849231210ad7bb47
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2023-06-05 19:54:34 -0400

    Improved code and do refactor over code /HomeDoctor component

--- src/lib/components/HomeDoctor.svelte
+++ src/lib/components/HomeDoctor.svelte
@@ -3,125 +3,126 @@
 	import Carousel from './Carousel.svelte';
 	import { areYouDoctor, masterKey } from '$lib/stores/store';
 	import { goto } from '$app/navigation';
-
+  
 	onMount(async () => {
-		const resp = await fetch(`/api/doctors/read?id=${$masterKey}`);
-		const js = await resp.json();
-		if (Object.keys(js).length === 0) {
-			goto(`/patient/${$masterKey}`);
-			return;
-		}
-		areYouDoctor.set(true);
+	  const resp = await fetch(`/api/doctors/read?id=${$masterKey}`);
+	  const js = await resp.json();
+	  if (Object.keys(js).length === 0) {
+		goto(`/patient/${$masterKey}`);
+		return;
+	  }
+	  areYouDoctor.set(true);
 	});
-</script>
-
-<Carousel />
-
-<div class="p-10 sm:ml-64">
+  </script>
+  
+  <Carousel />
+  
+  <div class="p-10 sm:ml-64">
 	<div class="overflow-x-auto w-full">
-		<table class="table w-full">
-			<tbody>
-				<tr>
-					<th>
-						<div class="font-bold">8:00 <br /> 9:00</div>
-					</th>
-					<td>
-						<div tabindex="0" class="collapse">
-							<div class="collapse-title text-xl font-medium">
-								<div class="font-bold">Gregory O'Connor appointment</div>
-								<div class="text-sm opacity-50">Face to face appointment</div>
-							</div>
-							<div>
-								<div class="collapse-content">
-									<p class="textarea textarea-bordered h-24">
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									</p>
-								</div>
-							</div>
-						</div>
-					</td>
-					<th>
-						<label class="swap">
-							<input type="checkbox" />
-							<div class="swap-on bg-success rounded-full">&nbsp;Accepted&nbsp;</div>
-							<div class="swap-off">OFF</div>
-						</label>
-					</th>
-				</tr>
-				<tr>
-					<th>
-						<div class="font-bold">9:00 <br /> 10:00</div>
-					</th>
-					<td>
-						<div tabindex="0" class="collapse">
-							<div class="collapse-title text-xl font-medium">
-								<div class="font-bold">Laura Mc.Gregor appointment</div>
-								<div class="text-sm opacity-50">Virtual appointment</div>
-							</div>
-							<div class="collapse-content">
-								<p class="textarea textarea-bordered h-24">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-								</p>
-							</div>
-						</div>
-					</td>
-					<th>
-						<label class="swap">
-							<input type="checkbox" />
-							<div class="swap-on bg-success rounded-full">&nbsp;Accepted&nbsp;</div>
-							<div class="swap-off">OFF</div>
-						</label>
-					</th>
-				</tr>
-				<tr>
-					<th>
-						<div class="font-bold">10:00 <br /> 11:00</div>
-					</th>
-					<td>
-						<div tabindex="0" class="collapse">
-							<div class="collapse-title text-xl font-medium">
-								<div class="font-bold">Nothing</div>
-								<div class="text-sm opacity-50" />
-							</div>
-							<div class="collapse-content">
-								<p class="textarea textarea-bordered h-24" />
-							</div>
-						</div>
-					</td>
-					<th>
-						<label class="swap">
-							<input type="checkbox" />
-							<div class="swap-on bg-success rounded-full">&nbsp;Accepted&nbsp;</div>
-							<div class="swap-off">OFF</div>
-						</label>
-					</th>
-				</tr>
-				<tr>
-					<th>
-						<div class="font-bold">11:00 <br /> 12:00</div>
-					</th>
-					<td>
-						<div tabindex="0" class="collapse">
-							<div class="collapse-title text-xl font-medium">
-								<div class="font-bold">Hart Hagerty</div>
-								<div class="text-sm opacity-50">Vrtual appointment</div>
-							</div>
-							<div class="collapse-content">
-								<p class="textarea textarea-bordered h-24">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-								</p>
-							</div>
-						</div>
-					</td>
-					<th>
-						<label class="swap">
-							<input type="checkbox" />
-							<div class="swap-on bg-success rounded-full">&nbsp;Accepted&nbsp;</div>
-							<div class="swap-off">OFF</div>
-						</label>
-					</th>
-				</tr>
-			</tbody>
-		</table>
+	  <table class="table w-full">
+		<tbody>
+		  <tr>
+			<th>
+			  <div class="font-bold">8:00 <br /> 9:00</div>
+			</th>
+			<td>
+			  <div tabindex="0" class="collapse">
+				<div class="collapse-title text-xl font-medium">
+				  <div class="font-bold">Gregory O'Connor appointment</div>
+				  <div class="text-sm opacity-50">Face to face appointment</div>
+				</div>
+				<div>
+				  <div class="collapse-content">
+					<p class="textarea textarea-bordered h-24">
+					  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					</p>
+				  </div>
+				</div>
+			  </div>
+			</td>
+			<th>
+			  <label class="swap">
+				<input type="checkbox" />
+				<div class="swap-on bg-success rounded-full">&nbsp;Accepted&nbsp;</div>
+				<div class="swap-off">OFF</div>
+			  </label>
+			</th>
+		  </tr>
+		  <tr>
+			<th>
+			  <div class="font-bold">9:00 <br /> 10:00</div>
+			</th>
+			<td>
+			  <div tabindex="0" class="collapse">
+				<div class="collapse-title text-xl font-medium">
+				  <div class="font-bold">Laura Mc.Gregor appointment</div>
+				  <div class="text-sm opacity-50">Virtual appointment</div>
+				</div>
+				<div class="collapse-content">
+				  <p class="textarea textarea-bordered h-24">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				  </p>
+				</div>
+			  </div>
+			</td>
+			<th>
+			  <label class="swap">
+				<input type="checkbox" />
+				<div class="swap-on bg-success rounded-full">&nbsp;Accepted&nbsp;</div>
+				<div class="swap-off">OFF</div>
+			  </label>
+			</th>
+		  </tr>
+		  <tr>
+			<th>
+			  <div class="font-bold">10:00 <br /> 11:00</div>
+			</th>
+			<td>
+			  <div tabindex="0" class="collapse">
+				<div class="collapse-title text-xl font-medium">
+				  <div class="font-bold">Nothing</div>
+				  <div class="text-sm opacity-50"></div>
+				</div>
+				<div class="collapse-content">
+				  <p class="textarea textarea-bordered h-24"></p>
+				</div>
+			  </div>
+			</td>
+			<th>
+			  <label class="swap">
+				<input type="checkbox" />
+				<div class="swap-on bg-success rounded-full">&nbsp;Accepted&nbsp;</div>
+				<div class="swap-off">OFF</div>
+			  </label>
+			</th>
+		  </tr>
+		  <tr>
+			<th>
+			  <div class="font-bold">11:00 <br /> 12:00</div>
+			</th>
+			<td>
+			  <div tabindex="0" class="collapse">
+				<div class="collapse-title text-xl font-medium">
+				  <div class="font-bold">Hart Hagerty</div>
+				  <div class="text-sm opacity-50">Virtual appointment</div>
+				</div>
+				<div class="collapse-content">
+				  <p class="textarea textarea-bordered h-24">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				  </p>
+				</div>
+			  </div>
+			</td>
+			<th>
+			  <label class="swap">
+				<input type="checkbox" />
+				<div class="swap-on bg-success rounded-full">&nbsp;Accepted&nbsp;</div>
+				<div class="swap-off">OFF</div>
+			  </label>
+			</th>
+		  </tr>
+		</tbody>
+	  </table>
 	</div>
-</div>
+  </div>
+  
