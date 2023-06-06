8e13f17906962af95c5cc148383f3321f2551e74
Ariel  Claros <ariel.claros.60091@usalesiana.edu.bo>
2023-06-06 11:07:03 -0400

    Refactored Header.svelte file

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import ArrowDownIcon from '../../icons/ArrowDownIcon.svelte';
-	import ArrowRightIcon from '../../icons/ArrowRightIcon.svelte';
 	import LoginIcon from '../../icons/LoginIcon.svelte';
-	import SearchIcon from '../../icons/SearchIcon.svelte';
-	import ListIcon from '../../icons/ListIcon.svelte';
 	import MedicalCheckLogo from '$lib/images/MedicalCheckLogo.png';
 	import { masterKey } from '$lib/stores/store';
+	import DropdownMenu from './DropdownMenu.svelte';
 
 	let id: string;
 	masterKey.subscribe((value) => (id = value));
@@ -14,22 +11,7 @@
 
 <nav class="fixed top-0 z-59 w-full navbar bg-base-100">
 	<div class="navbar-start">
-		<div class="dropdown">
-			<label tabindex="0" class="btn btn-ghost lg:hidden">
-				<ListIcon />
-			</label>
-			<ul
-				tabindex="0"
-				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-			>
-				<li tabindex="0">
-					<a class="justify-between" href="/doctor-list"> View all doctors </a>
-				</li>
-				<li tabindex="0">
-					<a class="justify-between"> About us </a>
-				</li>
-			</ul>
-		</div>
+		<DropdownMenu />
 		<a href={id === 'key-default' ? '/' : `/patient/${id}`}
 			><img src={MedicalCheckLogo} width="200px" alt="" /></a
 		>
